a512fc939bd8222ffefacfa764aa7b230cf9d465
dgrewal26 <dsingh26@studentmail.nait.ca>
2016-09-12 16:55:09 -0600

    Simple Console App

---
+++ StringQuack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace StringQuack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World";
+            Console.WriteLine(message.Quack());
+        }
+    }
+    public static class StringExtension
+    {
+        public static string Quack(this string self)
+        {
+            return self + "(quack)";
+        }
+    }
+}
